65b0ac277773a51fa0520bdfc013e675e275b7a9
Anmol Brar <brarsanmol@protonmail.com>
2022-01-18 23:28:21 -0500

    fix(item.service.interface.ts,item.service.ts): add the item service
    - add the interface for the item service.
    
    - this allows for abstraction between the ORM and the controller layer.
      - this decoupling might help in the future if the application is large
        and the decision is made to switch to another ORM.

---
+++ src/services/interfaces/item.service.interface.ts
@@ -0,0 +1,12 @@
+import { Item } from "@app/entities/item.entity"
+import { DeleteResult, UpdateResult } from "typeorm"
+
+export interface IItemService {
+  findByIdentifier(identifier: number): Promise<Item | undefined>
+
+  update(identifier: number, item: Partial<Item>): Promise<UpdateResult>
+
+  save(item: Item): Promise<Item>
+
+  delete(identifier: number): Promise<DeleteResult>
+}
---
+++ src/services/item.service.ts
@@ -0,0 +1,30 @@
+import { Item } from "@app/entities/item.entity"
+import { singleton } from "tsyringe"
+import { DeleteResult, getRepository, Repository, UpdateResult } from "typeorm"
+import { IItemService } from "./interfaces/item.service.interface"
+
+@singleton()
+export class ItemService implements IItemService {
+  constructor(
+    private readonly itemRepository: Repository<Item> = getRepository(Item)
+  ) {}
+
+  public async findByIdentifier(identifier: number): Promise<Item | undefined> {
+    return await this.itemRepository.findOne(identifier)
+  }
+
+  public async update(
+    identifier: number,
+    item: Partial<Item>
+  ): Promise<UpdateResult> {
+    return await this.itemRepository.update(identifier, item)
+  }
+
+  public async save(item: Item): Promise<Item> {
+    return await this.itemRepository.save(item)
+  }
+
+  public async delete(identifier: number): Promise<DeleteResult> {
+    return await this.itemRepository.delete(identifier)
+  }
+}
